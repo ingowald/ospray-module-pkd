59c11847c3903a40bf8f2080414e69590bcff7de
Will Usher <willusher.life@gmail.com>
2018-05-30 22:50:02 -0600

    working on parsing pkd xml files

--- sg/PKD.cpp
+++ sg/PKD.cpp
@@ -14,11 +14,13 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
-#undef NDEBUG
-
+#include <memory>
 #include "PKD.h"
 #include "ospcommon/xml/XML.h"
-
+#include "common/sg/importer/Importer.h"
+#include "common/sg/geometry/Spheres.h"
+#include "common/sg/common/Common.h"
+#include "ospcommon/xml/XML.h"
 #include "ospcommon/constants.h"
 
 namespace ospray {
@@ -259,9 +261,56 @@ namespace ospray {
     }
 #endif
 
+    void importPKD(std::shared_ptr<Node> world, const ospcommon::FileName fileName)
+    {
+      std::cout << "Loading PKDGeometry from " << fileName << std::endl;
+
+      // TODO: Read the radius from the XML file
+      // Map the binary file and hand it off to the PKDGeometry node
+      // as a shared data pointer.
+      // We then also need to make a transfer function in the case that the
+      // data has attributes
+      auto doc = xml::readXML(fileName);
+      const std::string binFileName = fileName.str() + "bin";
+      const unsigned char *binBasePtr = mapFile(binFileName);
+      if (!binBasePtr) {
+        std::cout << "Failed to load corresponding pkdbin file for " << fileName.str() << "\n";
+        throw std::runtime_error("Failed to load corresponding pkdbin file for "
+                                  + fileName.str());
+      }
+      auto geom = createNode(fileName.str(), "PKDGeometry")->nodeAs<PKDGeometry>();
+
+      const xml::Node &pkdNode = doc->child[0].child[0];
+      if (pkdNode.name != "PKDGeometry") {
+        std::cout << "failed to find PKDGeometry child node\n";
+        throw std::runtime_error("failed to find PKDGeometry child node");
+      }
+      for (const xml::Node &e : pkdNode.child) {
+        // TODO: How are attributes stored again? I forgot, need to fix
+        // the converter and make some examples
+        if (e.name == "position") {
+          std::cout << "pos: ofs: " << e.getProp("ofs")
+            << ", count = "  << e.getProp("count")
+            << ", format = " << e.getProp("format")
+            << "\n";
+        } else if (e.name == "radius") {
+          geom->createChild("radius", "float", std::stof(e.content));
+        }
+      }
+
+      auto model = createNode(fileName.str() + "_model", "Model");
+      model->add(geom);
+
+      auto instance = createNode(fileName.str() + "_instance", "Instance");
+      instance->setChild("model", model);
+      model->setParent(instance);
+      world->add(model);
+    }
+
     OSP_REGISTER_SG_NODE(PKDGeometry);
+
+    OSPSG_REGISTER_IMPORT_FUNCTION(importPKD, pkd);
 
   } // ::ospray::sg
 } // ::ospray
-
 
